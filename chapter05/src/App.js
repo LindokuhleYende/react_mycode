ba1211ecf8da3e6506dc91922aaa682fc7484613
Lindokuhle <yendelindokuhle2@gmail.com>
2025-05-24 17:33:06 +0200

    Update App.js

--- chapter05/src/App.js
+++ chapter05/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import Products from './Products';
 import JumboTronComponent from './JumboTronComponent';
+import Task from "./taskTest";
 
 class App extends Component {
   render() {
@@ -8,9 +9,10 @@ class App extends Component {
       <div className='App'>
         {/* <Products /> */}
         <JumboTronComponent>
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
         </JumboTronComponent>
+        <Task />
       </div>
     );
   }
