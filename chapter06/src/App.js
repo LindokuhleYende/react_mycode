0fcc995dab4f97c0ee16f2dab3408c428defd143
Lindokuhle <yendelindokuhle2@gmail.com>
2025-05-26 10:29:44 +0200

    Getting Data from RestFul Api's

--- chapter06/src/App.js
+++ chapter06/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import UserForm from './UserForm';
+import GitHub from './GitHub';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <UserForm />
+        <GitHub />
       </div>
     );
   }
